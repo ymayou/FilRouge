1a655d525bd2145828484f8862dce81e786ba4e4
Florian <florian.garcia.cpe@gmail.com>
2015-02-05 10:08:25 +0100

    formulaire de connexion

--- src/index.php
+++ src/index.php
@@ -1,18 +1,36 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>Acupuncture</title>
+    <head>
+        <meta charset="utf-8">
+        <title>Acupuncture</title>
 
-    <link rel="stylesheet" type="text/css" href="../includes/css/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+        <link rel="stylesheet" type="text/css" href="../includes/css/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
 
-<body>
-<?php
-    include_once("menu.php");
-?>
-</body>
+    <body>
+        <?php
+        include_once("menu.php");
+        ?>
+        <div class="content">
+            <fieldset>
+                <legend>Se connecter</legend>
+                <form action="#" method="POST">
+                    <p>
+                        <label for="nom">Login : </label>
+                        <input name="nom" id="nom" type="text" placeholder="Votre pseudo"/>
+                    </p>
+                    <p>
+                        <label for="password">Mot de passe : </label>
+                        <input name="password" id="password" type="password" placeholder="Mot de passe"/>
+                    </p>
+                    <p>
+                        <button type="submit">Se connecter</button>
+                    </p>
+                </form>
+            </fieldset>
+        </div>
+    </body>
 
 </html>
 
