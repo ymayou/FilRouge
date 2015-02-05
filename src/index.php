fd24739a7f808e5d99d0b69cf18cba6766c2d641
Yacine-M <mayou.yacine@gmail.com>
2015-02-05 09:25:16 +0100

    templae html

--- src/index.php
+++ src/index.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tfk
- * Date: 05/02/2015
- * Time: 09:11
- */ 
+include_once("menu.php");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Acupuncture</title>
+
+    <link rel="stylesheet" type="text/css" href="../includes/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+</body>
+
+</html>
+
--- src/menu.php
+++ src/menu.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tfk
- * Date: 05/02/2015
- * Time: 09:12
- */ 
