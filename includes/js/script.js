78a3ccd25d29da82a445e07df229adf5cc81f09d
Yacine-M <mayou.yacine@gmail.com>
2015-02-24 13:15:29 +0100

    Modification architecture

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
     var url = window.location.href;
     $("#menu a").each(function() {
-        if(url == (this.href)) {
+        console.log("url : " + url);
+        console.log("href")
+        if(url == (this.href))
             $(this).closest("li").addClass("active");
-        }
+        else
+            $('a[href^="index.php"]').closest("li").addClass("active");
     });
 });
