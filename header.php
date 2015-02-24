2f5bb25e3cedb29475b05126bb420b8101cf659e
Yacine-M <mayou.yacine@gmail.com>
2015-02-24 13:16:41 +0100

    Modification architecture

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Acupuncture</title>
+    <link rel="stylesheet" type="text/css" href="./includes/css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="./includes/js/script.js"></script>
+</head>
+<body>
+    <?php
+        include_once("menu.php");
+    ?>
---
+++ menu.php
@@ -0,0 +1,19 @@
+<div id="menu">
+    <ul>
+        <li>
+            <a href="index.php">Accueil</a>
+        </li>
+        <li>
+            <a href="profile.php">Profile</a>
+        </li>
+        <li>
+            <a href="pathologie.php">Pathologie</a>
+        </li>
+        <li>
+            <a href="#">page 4</a>
+        </li>
+        <li>
+            <a href="#">page 5</a>
+        </li>
+    </ul>
+</div>
