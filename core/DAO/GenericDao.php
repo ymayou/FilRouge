db7ea84012932fce252d28e288de99feeeb209cf
Florian <florian.garcia.cpe@gmail.com>
2015-02-26 16:11:29 +0100

    Ce gars a fait a fait une DAO de base; vous serez époustouflés quand vous saurez comment il a fait !

---
+++ core/DAO/GenericDao.php
@@ -0,0 +1,65 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once 'core/DAO/Connexion.php';
+class GenericDao {
+     protected $connexion;
+     var $tableName;
+     protected $idColumn;
+     
+     public function __construct($cnx,$tableName,$idColumn) {
+         if($cnx == null){
+             $this->connexion = Connexion::getInstance();
+         } else {
+             $this->connexion = $cnx;
+         }
+         $this->tableName = $tableName;
+         $this->idColumn = $idColumn;
+     }
+     
+     public function findAll($sortingExpr,$from,$to){
+         $sql = "SELECT * FROM ".$this->tableName;
+         if($sortingExpr != null){
+             $sql.=" ORDER BY ".$sortingExpr;
+         }
+         if($from !=null && $to != null){
+             $sql .=" LIMIT ".$from.",".$to;
+         }
+         $requete = $this->connexion->prepare($sql);
+         if($requete->execute()){
+             while($donnees = $requete->fetchAll()){
+                 return $donnees;
+             }
+         } else {
+             return null;
+         }
+     }
+     
+     public function findById($pkVal){
+         $sql = "SELECT * FROM ".$this->tableName." WHERE ".$this->idColumn." = :myVal";
+         $requete = $this->connexion->prepare($sql);
+         $requete->bindValue(':myVal', $pkVal);
+         if($requete->execute()){
+             if($donnees = $requete->fetch()){
+                 return $donnees;
+             } else {
+                 return null;
+             }
+         }
+     }
+     
+     public function deleteById($pkVal){
+         $sql = "DELETE FROM ".$this->tableName." WHERE ".$this->idColumn." = :myVal";
+         $requete = $this->connexion->prepare($sql);
+         $requete->bindValue(':myVal', $pkVal);
+         if($requete->execute()){
+             return true;
+         } else {
+             return false;
+         }
+     }
+}
