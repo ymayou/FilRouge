80e05fecd5e9b9ef5553fd5ea1e84e36e60151a2
Yacine-M <mayou.yacine@gmail.com>
2015-02-26 16:14:03 +0100

    commit

--- includes/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var url = window.location.href;
-    $("#menu a").each(function() {
-        console.log("url : " + url);
-        console.log("href : " + this.href);
-        if(url == (this.href)){
-            $(this).closest("li").addClass("active");}
-    });
-});
