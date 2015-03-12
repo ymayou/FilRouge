ea53f502fcfcd958c23e01e06f4ca376dced9cb8
Florian <florian.garcia.cpe@gmail.com>
2015-03-12 16:36:25 +0100

    liste pathologie

--- controllers/pathologie.php
+++ controllers/pathologie.php
@@ -1,6 +1,12 @@
 <?php
     require_once 'core/DAO/MeridienDao.php';
+    require_once 'core/DAO/PathologieDao.php';
     $cnx = null;
     $meridienDao = new MeridienDao($cnx);
     $listeCarac = $meridienDao->findAllCaracteristique('', '', '');
     $smarty->assign('listeCarac', $listeCarac);
+    
+    $pathologieDao = new PathologieDao($cnx);
+    $listePatho = $pathologieDao->listePatho();
+    //print_r($pathologieDao);
+    $smarty->assign('listePatho', $listePatho);
--- core/DAO/PathologieDao.php
+++ core/DAO/PathologieDao.php
@@ -7,7 +7,23 @@
  */
 require_once 'core/DAO/GenericDao.php';
 class PathologieDao extends GenericDao {
+    public $tableName;
+    
     public function __construct($cnx){
+        $this->tableName = 'patho';
         parent::__construct($cnx, "patho", "idP");
+    }
+    
+    public function listePatho() {
+        $sql = "SELECT * FROM ".$this->tableName;
+         $requete = $this->connexion->prepare($sql);
+         if($requete->execute()){
+             while($donnees = $requete->fetchAll()){
+                 //print_r($donnees);
+                 return $donnees;
+             }
+         } else {
+             return null;
+         }
     }
 }
