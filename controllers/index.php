8ce50d0b6ffd66292e437aa84595a975a23d243f
Yacine-M <mayou.yacine@gmail.com>
2015-03-12 14:26:23 +0100

    test git

--- controllers/index.php
+++ controllers/index.php
@@ -2,4 +2,6 @@
 require_once 'core/DAO/PathologieDao.php';
     $cnx = null;
     $test = new PathologieDao($cnx);
+    if(isset($_POST["connexion"]))
+        header("location: ?page=pathologie");
 ?>
